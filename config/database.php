d2442e8ce538ce6b0568f66cd3a23c4f7f132d24
kangaman <saefulkinfo@gmail.com>
2025-11-18 14:43:20 +0700

    Create database.php

---
+++ config/database.php
@@ -0,0 +1,24 @@
+<?php
+// Pengaturan Koneksi Database
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'xltmgqsc_osint');
+define('DB_USER', 'xltmgqsc_osint'); // Ganti dengan username database Anda
+define('DB_PASS', 'H($vNJBC0@Afv6n!');     // Ganti dengan password database Anda
+define('DB_CHARSET', 'utf8mb4');
+
+// Opsi untuk koneksi PDO
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+// Membuat koneksi PDO
+try {
+    $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET, DB_USER, DB_PASS, $options);
+} catch (\PDOException $e) {
+    // Jika koneksi gagal, hentikan aplikasi dan tampilkan pesan error
+    // Di lingkungan produksi, error ini sebaiknya dicatat (log) bukan ditampilkan ke pengguna
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+?>
